41f36250dc7be63fc6b40c2c304092e9fc3ce3f0
jjhbriggs <jjhbriggs@gmail.com>
2020-05-03 03:19:10 +0000

    Fixed __init__.py for polls

--- django-polls/polls/__init__.py
+++ django-polls/polls/__init__.py
@@ -1,7 +1,1 @@
-'''from .admin import *
-from .apps import *
-from .models import *
-from .tests import *
-from .urls import *
-from .views import *
-'''
+
---
+++ django-polls/polls/tmp.py
@@ -0,0 +1,5 @@
+default_app_config = 'polls.apps.PollsConfig'
+from .models import *
+from .tests import *
+from .urls import *
+from .views import *
